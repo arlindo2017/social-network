9444090674e332515b97d9322ebeb34f7b41bf9f
Arlindo Pinto <perensen@gmail.com>
2023-05-03 17:02:07 -0400

    updated POST routed to updated User

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require("../models");
+const { Thought, User } = require("../models");
 
 module.exports = {
   // Get all thoughts
@@ -24,15 +24,27 @@ module.exports = {
       })
       .catch((err) => res.status(500).json(err));
   },
-  // Create a new Thought
+  // Create a new Thought and add thought to user thoughts
   createThought(req, res) {
     Thought.create(req.body)
+      .then((createdthought) => {
+        if (!createdthought) {
+          res.status(404).json({ message: "No user with that ID was found!" });
+        } else {
+          return User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: createdthought._id } },
+            { new: true }
+          );
+        }
+      })
       .then((thought) => res.json(thought))
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
+
   // Update a Thought
   updateThought(req, res) {
     Thought.findOneAndUpdate(
@@ -68,6 +80,17 @@ module.exports = {
       { $addToSet: { reactions: req.body } },
       { runValidators: true, new: true }
     )
+      .then((createdReaction) => {
+        if (!createdReaction) {
+          res.status(404).json({ message: "No user with that ID was found!" });
+        } else {
+          return User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: createdReaction._id } },
+            { new: true }
+          );
+        }
+      })
       .then((thought) => {
         if (!thought) {
           return res.status(404).json({ message: "No thought with that ID" });
